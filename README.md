a73a2a327388da7d109c4d42fdfbf04ef51b8a66
Juan Pablo Botero <juan.boterotorres@utah.edu>
2024-11-12 12:13:40 -0700

    Added Slurm Job files and trained models

--- README.md
+++ README.md
@@ -20,3 +20,44 @@
 ## NeuroBench Information: 
 * Neurobench expects to recieve an snnTorch model, for such we will use the [NIR repo](https://github.com/neuromorphs/NIR) (Neuromorphic Intermediate Representation) which allows us to convert a Nengo model to a snnTorch model.
 
+## Running Jobs on CHPC: 
+### Load module:
+```
+  module load miniforge3/latest
+```
+
+### Utilizing 'notchpeak-gpu' in interactive mode:
+* Check for free GPUs in the partition: 
+  ```
+    freegpus -p notchpeak-gpu
+  ```
+
+* Request GPU resources interactively:
+  ```
+    salloc -n 1 -N 1 -t 3:00:00 -p notchpeak-gpu -A notchpeak-gpu --gres=gpu:3090:1 --mem=32G
+  ```
+  - n 1: Requests 1 task (CPU)
+  - N 1: Requests 1 node for the job
+  - t 3:00:00: Sets the time limit to 3hr.
+  - gres=gpu:t4:1: Requests 1 NVIDIA T4 GPU
+
+* Run a batch job: 
+  ```
+    sbatch <path to .slurm file>
+  ```
+  Useful commands: 
+    - scontrol show job <job ID>
+    - squeue -u u1437983
+
+
+### Further CHCP Resources: 
+  * Running Jobs on GPUs via Slurm: [CHCP link](https://www.chpc.utah.edu/documentation/software/slurm-gpus.php)
+  * Scheduling Jobs at the CHPC with Slurm: [CHCP link](https://www.chpc.utah.edu/documentation/software/slurm.php#aliases)
+  * GPUs and Accelerators at the CHPC: [CHCP link](https://www.chpc.utah.edu/documentation/guides/gpus-accelerators.php)
+  * Youtube channel: [Youtube](https://www.youtube.com/@centerforhighperformanceco2744)
+
+
+
+
+  
+
--- train_GRU_RNN.py
+++ train_GRU_RNN.py
@@ -12,92 +12,133 @@ import nengo
 from util import get_data_BIOCAS
 from data_loader_utils import Batch_Dataset
 from sklearn.model_selection import train_test_split
+import argparse
+import datetime
+
+def main(epochs= 10, model_path=None, log_dir=None):
+    # Configuration
+    input_size = 192 # Electrodes
+    output_size = 2 # X and Y velocity
+    latent_size = 128 # Arbitrary
+    batch_size = 32 
+    epochs = 15
+    learning_rate = 1e-3 # Arbitrary
+    seq_len = 1000  # 1s sequence length (arbitrary)
+
+    # Model ID = {date}-GRU-regressor-ls{latent_size}-lr{learning_rate}-bs{batch_size}-sl{seq_len}
+    date = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    model_id = f"{date}-GRU-regressor-ls{latent_size}-lr{learning_rate}-bs{batch_size}-sl{seq_len}-{epochs}epochs"
+    log_dir = f"./Training_logs/{model_id}"
+    os.makedirs(log_dir, exist_ok=True)
+
+    # Initialize TensorBoard SummaryWriter
+    writer = SummaryWriter(log_dir=log_dir)
+
+    # Check if GPU is available
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print("Using device: {}".format(torch.cuda.get_device_name(torch.cuda.current_device())))
+
+    # Initialize Model
+    model = GRU_RNN(latent_size=latent_size)
+    model.init_model(input_size=input_size, output_size=output_size)
+    model.to(device)
+
+    # Define Loss and Optimizer
+    criterion = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+    # Load model checkpoint if provided
+    if model_path is not None:
+        checkpoint = torch.load(model_path)
+        model.load_state_dict(checkpoint["model_state_dict"])
+        optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
+        start_epoch = checkpoint["epoch"]
+        loss = checkpoint["loss"]
+        print(f"Loaded model checkpoint from epoch {start_epoch} with loss {loss}")
+    
+    else:
+        start_epoch = 0
+
+    # Load data 
+    file_name = "indy_20160407_02.mat"
+    inputfile = os.path.join("Dataset", "NHP Reaching Sensorimotor Ephys", file_name)
+    cur_pos, spike_data, cur_vel = get_data_BIOCAS(inputfile)
+    # Calculate instantaneous firing rate from spike data applying synaptic filter
+    synapse = nengo.synapses.Lowpass(tau=0.7) # tau is taken from Naive model (L2 reg linear regression) optimal value 
+    FR = synapse.filt(spike_data)
+
+    # Split dataset 
+    train_FR, test_FR, train_vel, test_vel = train_test_split(FR.T, cur_vel.T, test_size=0.5,shuffle=False) # 50% train, 50% test
+    val_FR, train_FR, val_cursor, train_vel = train_test_split(train_FR, train_vel, test_size=0.75,shuffle=False) # 25% val, 75% train
+    train_FR_tensor = torch.tensor(train_FR.T, dtype=torch.float32).to(device)
+    train_vel_tensor = torch.tensor(train_vel.T, dtype=torch.float32).to(device)
+
+    train_data = Batch_Dataset(train_FR_tensor, train_vel_tensor, seq_len)
+    train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
 
 
-# Configuration
-input_size = 192 # Electrodes
-output_size = 2 # X and Y velocity
-latent_size = 128 # Arbitrary
-batch_size = 32 
-epochs = 3
-learning_rate = 1e-3 # Arbitrary
-seq_len = 1000  # 1s sequence length (arbitrary)
-log_dir = "./logs"
-os.makedirs(log_dir, exist_ok=True)
-
-# Initialize TensorBoard SummaryWriter
-writer = SummaryWriter(log_dir=log_dir)
-
-# Initialize Model
-model = GRU_RNN(latent_size=latent_size)
-model.init_model(input_size=input_size, output_size=output_size)
-
-# Define Loss and Optimizer
-criterion = nn.MSELoss()
-optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-
-
-# Load data 
-inputfile = "Dataset\\NHP Reaching Sensorimotor Ephys\\indy_20160407_02.mat"
-cur_pos, spike_data, cur_vel = get_data_BIOCAS(inputfile)
-# Calculate instantaneous firing rate from spike data applying synaptic filter
-synapse = nengo.synapses.Lowpass(tau=0.7) # tau is taken from Naive model (L2 reg linear regression) optimal value 
-FR = synapse.filt(spike_data)
-
-# Split dataset 
-train_FR, test_FR, train_vel, test_vel = train_test_split(FR.T, cur_vel.T, test_size=0.5,shuffle=False) # 50% train, 50% test
-val_FR, train_FR, val_cursor, train_vel = train_test_split(train_FR, train_vel, test_size=0.75,shuffle=False) # 25% val, 75% train
-train_FR_tensor = torch.tensor(train_FR.T, dtype=torch.float32)
-train_vel_tensor = torch.tensor(train_vel.T, dtype=torch.float32)
-
-train_data = Batch_Dataset(train_FR_tensor, train_vel_tensor, seq_len)
-train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
-
-
-# Training loop
-for epoch in range(epochs):
-    epoch_loss = 0.0
-    for batch in tqdm(train_loader, desc=f"Epoch {epoch + 1}/{epochs}"):
-        inputs, targets = batch
-        batch_size, seq_len, _ = inputs.size()
-        
-        # Initialize hidden state
-        hidden = model.init_hidden(batch_size)
-        
-        # Process each time step
-        optimizer.zero_grad()
-        loss = 0.0
-        for t in range(seq_len):
-            input_t = inputs[:, t, :]
-            target_t = targets[:, t, :]
+    # Training loop
+    for epoch in range(epochs-start_epoch):
+        epoch += start_epoch
+        epoch_loss = 0.0
+        for batch in tqdm(train_loader, desc=f"Epoch {epoch + 1}/{epochs}"):
+            inputs, targets = batch
+            inputs, targets = inputs.to(device), targets.to(device)  # Move data to GPU
+            batch_size, seq_len, _ = inputs.size()
             
-            # Forward pass
-            output_t, hidden = model(input_t, hidden)
-            loss += criterion(output_t, target_t)
+            # Initialize hidden state
+            hidden = model.init_hidden(batch_size)
+            
+            # Process each time step
+            optimizer.zero_grad()
+            loss = 0.0
+            for t in range(seq_len):
+                input_t = inputs[:, t, :]
+                target_t = targets[:, t, :]
+                
+                # Forward pass
+                output_t, hidden = model(input_t, hidden)
+                loss += criterion(output_t, target_t)
+            
+            # Backpropagation and optimization
+            loss.backward()
+            optimizer.step()
+            
+            # Accumulate loss
+            epoch_loss += loss.item() / seq_len
         
-        # Backpropagation and optimization
-        loss.backward()
-        optimizer.step()
+        # Average loss per batch
+        epoch_loss /= len(train_loader)
         
-        # Accumulate loss
-        epoch_loss += loss.item() / seq_len
-    
-    # Average loss per batch
-    epoch_loss /= len(train_loader)
-    
-    # Log the loss to TensorBoard
-    writer.add_scalar("Loss/train", epoch_loss, epoch)
-    print(f"Epoch [{epoch+1}/{epochs}], Loss: {epoch_loss:.4f}")
+        # Log the loss to TensorBoard
+        writer.add_scalar("Loss/train", epoch_loss, epoch)
+        print(f"Epoch [{epoch+1}/{epochs}], Loss: {epoch_loss:.4f}")
 
-    # Save training loss to JSON file for additional logging if desired
-    with open(os.path.join(log_dir, "training_log.json"), "a") as f:
-        f.write(json.dumps({"epoch": epoch + 1, "loss": epoch_loss}) + "\n")
+        # Save training loss to JSON file for additional logging if desired
+        with open(os.path.join(log_dir, "training_log.json"), "a") as f:
+            f.write(json.dumps({"epoch": epoch + 1, "loss": epoch_loss}) + "\n")
 
-# Save model
-torch.save(model.state_dict(), os.path.join(log_dir, "gru_rnn_model.pth"))
+        # Save model checkpoint
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'loss': epoch_loss,
+        }, os.path.join(log_dir, f"gru_rnn_checkpoint.pth"))
 
-# Close the TensorBoard writer
-writer.close()
 
-# To monitor training, run this command in the terminal:
-# tensorboard --logdir=./logs
+    # Close the TensorBoard writer
+    writer.close()
+
+    # To monitor training, run this command in the terminal:
+    # tensorboard --logdir=./logs
+
+if __name__ == "__main__":
+    # read arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_path", type=str, default=None, help="Path to model checkpoint")
+    parser.add_argument("--epochs", type=int, default=15, help="Number of epochs to train")
+    args = parser.parse_args()
+
+    # Run training loop
+    main(epochs=args.epochs, model_path=args.model_path)
